308acd4dd1d1d21c538e74b2769313e261bcdffc
Heriberto Reyes Esparza <hery.chemo@gmail.com>
2017-01-16 12:53:22 -0600

    Creation of initial object.
    This is just a simple object.
    and a test message.

---
+++ index.py
@@ -0,0 +1,14 @@
+
+class MyObject():
+	"""docstring for MyObject"""
+	def __init__(self, name):
+		super(MyObject).__init__()
+		self.name = name
+	
+	def say_name(self):
+		return self.name
+
+print("Hi Friends");
+
+obj = MyObject("Object #1");
+
